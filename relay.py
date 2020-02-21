d637695152664e7001d191feebd995568158964d
Ledger West <ledger.west@gmail.com>
2020-02-20 21:40:24 -0500

    feat: update relay handler

--- relay.py
+++ relay.py
@@ -11,19 +11,24 @@ PASSWORD = os.environ['PASSWORD']
 
 
 def main(event, context):
-    if event['httpMethod'] == 'GET':
-        #response = requests.get(PINNACLE_URL)
+    if event['body']['method'] == 'GET':
         logger.info(event)
+        response = requests.get('{}{}'.format(HOST_URL, event['body']['path']),
+                                auth=(USERNAME, PASSWORD),
+                                params=event['body']['params'])
 
-    if event['httpMethod'] == 'POST':
-        #response = requests.post(PINNACLE_URL, event['body'])
+    if event['body']['method'] == 'POST':
         logger.info(event)
+        response = requests.post('{}{}'.format(HOST_URL, event['body']['path']),
+                                auth=(USERNAME, PASSWORD),
+                                data=event['body']['body'],
+                                headers = {'content-type': 'application/json'})
 
     #print(event)
     #logger.info(event)
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(event)
-    }
+    #response = {
+    #    "statusCode": 200,
+    #    "body": json.dumps(event)
+    #}
 
     return response
