d47687f25afbec237d78eb8b381dd167045f2a5e
Ledger West <ledger.west@gmail.com>
2020-02-21 02:37:53 -0500

    fix: response content format

--- relay.py
+++ relay.py
@@ -12,16 +12,21 @@ PASSWORD = os.environ['PASSWORD']
 
 def main(event, context):
     if event['httpMethod'] == 'GET':
-        response = requests.get('{}{}'.format(HOST_URL, event['path']),
+        resp = requests.get('{}{}'.format(HOST_URL, event['path']),
                                 auth=(USERNAME, PASSWORD),
                                 params=event['queryStringParameters'])
 
     if event['httpMethod'] == 'POST':
-        response = requests.post('{}{}'.format(HOST_URL, event['path']),
+        resp = requests.post('{}{}'.format(HOST_URL, event['path']),
                                 auth=(USERNAME, PASSWORD),
                                 data=json.dumps(event['body']),
                                 headers={'content-type': 'application/json'})
 
     logger.info(event)
+
+    response = {
+        'statusCode': resp.status_code,
+        'body': json.dumps(resp.json()['data'])
+    }
 
     return response
